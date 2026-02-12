0a0fa5cce6479962fa1d29726dc332f62fa520f5
AndreaMerella <71179576+AndreaMerella@users.noreply.github.com>
2026-02-12 09:01:12 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'vngrd-v1';
+const assets = ['./indexbackup.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
